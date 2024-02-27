1754946515893bc90c522bcc9b8c2e1234b8d472
Nicolas Bonnet <mail@nicolasbon.net>
2024-02-27 22:12:12 +0100

    Move log level to debug

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def _positive_int(s: any):
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO,
+    logging.basicConfig(level=logging.DEBUG,
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
 
