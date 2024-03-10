a274d62e92b2bcd4a4429bc3867c89d34a6c7d18
Nicolas Bonnet <mail@nicolasbon.net>
2024-03-10 16:09:57 +0100

    Remove unused code

--- sqlite-compactor/compactor.py
+++ sqlite-compactor/compactor.py
@@ -1,4 +1,3 @@
-import datetime
 import logging
 import os
 import sqlite3
@@ -9,12 +8,6 @@ from query_builder import QueryBuilder, compact_table
 from ulid import ULID
 
 logger = logging.getLogger(__name__)
-
-
-class NotEnoughRowsToCompact(Exception):
-    def __init__(self, row_count: int):
-        super().__init__(f"Table has {row_count} rows, not enough to compact.")
-        self.row_count = row_count
 
 
 class Compactor:
@@ -90,21 +83,3 @@ class Compactor:
     def _vacuum_database(self) -> None:
         with sqlite3.connect(self.sqlite_database_path) as con:
             con.execute("VACUUM")
-
-
-def _create_compaction_table(sqlite_con_factory: sqlite3.Connection):
-    with sqlite_con_factory as con:
-        statement = "CREATE TABLE IF NOT EXISTS compactions (id TEXT, timestamp_ms INTEGER, file_name TEXT, table_name TEXT)"
-        con.execute(statement)
-
-
-def _get_unix_timestamp_ms(t: datetime.datetime) -> int:
-    ts = (t - datetime.datetime(1970, 1, 1)) / datetime.timedelta(milliseconds=1)
-    return int(ts)
-
-
-def _attempt_to_delete(path: str) -> None:
-    try:
-        os.remove(path)
-    except OSError:
-        pass
